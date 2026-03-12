31a2517a2e96147ecdae82007ed0c40f843ef54a
Shubham Raj <shubhamraj2202@gmail.com>
2026-03-13 01:06:49 +0900

    Initial Commit

---
+++ Kaizen OS/Kaizen_OSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kaizen_OSApp.swift
+//  Kaizen OS
+//
+//  Created by Shubham Raj on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Kaizen_OSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
